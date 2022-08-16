1133a5d1f0e03d3c88b0c052894d30d046ffda1c
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2022-08-16 09:54:38 +0700

    Update and rename health_checks.py to InteractOS/Week1/health_checks.py

--- InteractOS/Week1/health_checks.py
+++ InteractOS/Week1/health_checks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import shutil
 import psutil
+from network import *
+
 def check_disk_usage(disk):
     """Verifies that there's enough free space on disk"""
     du = shutil.disk_usage(disk)
@@ -9,9 +11,11 @@ def check_disk_usage(disk):
 def check_cpu_usage():
     """Verifies that there's enough unused CPU"""
     usage = psutil.cpu_percent(1)
-    return usage > 75
+    return usage < 75
 # If there's not enough disk, or not enough CPU, print an error
 if not check_disk_usage('/') or not check_cpu_usage():
     print("ERROR!")
-else:
+elif check_localhost() and check_connectivity():
     print("Everything ok")
+else:
+    print("Network checks failed")
